039be11b8de9e1198d6068a885cae0d2d4cdd28e
chryswds <dybas.chrys@gmail.com>
2026-04-02 10:48:00 +0000

    first commit

---
+++ src/test/java/cl/spring/record/RecordApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.spring.record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
