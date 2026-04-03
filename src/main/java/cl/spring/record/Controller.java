020dddcf932f6f49ef73cfd244cf365f85c2eb78
chryswds <dybas.chrys@gmail.com>
2026-04-03 10:33:00 +0000

    first route

---
+++ src/main/java/cl/spring/record/Controller.java
@@ -0,0 +1,17 @@
+package cl.spring.record;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hello World!";
+    }
+
+
+}
