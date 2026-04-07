9655033c17186923c1a3a547f094d56364859da0
chryswds <dybas.chrys@gmail.com>
2026-04-07 18:59:00 +0000

    adding lombok - @AllArgs and @NoArgs constructors - @Data for getters and setters

--- src/main/java/cl/spring/record/Person/PersonModel.java
+++ src/main/java/cl/spring/record/Person/PersonModel.java
@@ -3,6 +3,9 @@ package cl.spring.record.Person;
 import cl.spring.record.Tasks.TasksModel;
 import jakarta.persistence.*;
 import cl.spring.record.Tasks.TasksModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -10,52 +13,17 @@ import java.util.List;
 // JPA - Java Persistence API
 @Entity
 @Table(name = "tb_record")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private int age;
-
-
-
     @ManyToOne
     @JoinColumn(name = "tasks_id") // Foreigner Key
     private TasksModel tasks;
-
-
-    public PersonModel() {
-    }
-
-    public PersonModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
--- src/main/java/cl/spring/record/Tasks/TasksModel.java
+++ src/main/java/cl/spring/record/Tasks/TasksModel.java
@@ -3,20 +3,23 @@ package cl.spring.record.Tasks;
 
 import cl.spring.record.Person.PersonModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TasksModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String time_requested;
-
     @OneToMany(mappedBy = "tasks")
     private List<PersonModel> person;
-
 }
