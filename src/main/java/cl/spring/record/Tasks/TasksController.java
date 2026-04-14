d50bb88b0c647fed37756ec9619c0c0fe8dec3c7
chryswds <dybas.chrys@gmail.com>
2026-04-14 15:32:00 +0000

    feat: creates routes for tasks controller - mock

--- src/main/java/cl/spring/record/Tasks/TasksController.java
+++ src/main/java/cl/spring/record/Tasks/TasksController.java
@@ -1,4 +1,29 @@
 package cl.spring.record.Tasks;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("tasks")
 public class TasksController {
+
+
+    @GetMapping("/show")
+    public String showTasks(){
+        return "All tasks";
+    }
+
+    @PostMapping("/add")
+    public String addTask(){
+        return "Task added";
+    }
+
+    @PutMapping("/update")
+    public String updateTask(){
+        return "Task updated";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteTask(){
+        return "Task deleted";
+    }
 }
