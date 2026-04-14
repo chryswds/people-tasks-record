a2e75c0e285eac0091973d6dd74696c66b21d07f
chryswds <dybas.chrys@gmail.com>
2026-04-14 11:11:00 +0000

    feat: updates controller with routes mock

--- src/main/java/cl/spring/record/Person/PersonController.java
+++ src/main/java/cl/spring/record/Person/PersonController.java
@@ -1,8 +1,6 @@
 package cl.spring.record.Person;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,37 @@ public class PersonController {
     @GetMapping("/welcome")
     public String welcome(){
         return "Hello World!";
+    }
+
+
+    // Add person
+    @PostMapping("/add")
+    public String addPerson(){
+        return "Person added";
+    }
+
+    // Show every person registered
+    @GetMapping("/all")
+    public String showAll(){
+        return "All persons";
+    }
+
+    // Search Person by ID
+    @GetMapping("/personbyid")
+    public String showById(){
+        return "Person by id";
+    }
+
+    // Update Person
+    @PutMapping("/updatebyid")
+    public String updatePersonById(){
+        return "Person updated";
+    }
+
+    // Delete Person
+    @DeleteMapping("/deletebyid")
+    public String deletePersonById(){
+        return "Person deleted";
     }
 
 
