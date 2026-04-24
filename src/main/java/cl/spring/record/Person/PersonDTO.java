e0d8dde2496b01457bc8605eefe70c99de66f7a3
chryswds <dybas.chrys@gmail.com>
2026-04-24 12:01:00 +0000

    chore: adds DTO version of the entity and maps through the mapper

--- src/main/java/cl/spring/record/Person/PersonDTO.java
+++ src/main/java/cl/spring/record/Person/PersonDTO.java
@@ -1,4 +1,21 @@
 package cl.spring.record.Person;
 
+import cl.spring.record.Tasks.TasksModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String imgUrl;
+    private int age;
+    private String surname;
+    private TasksModel tasks;
+
 }
--- src/main/java/cl/spring/record/Person/PersonMapper.java
+++ src/main/java/cl/spring/record/Person/PersonMapper.java
@@ -1,4 +1,31 @@
 package cl.spring.record.Person;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PersonMapper {
+
+    public PersonModel map(PersonDTO personDTO) {
+        PersonModel personModel = new PersonModel();
+        personModel.setId(personDTO.getId());
+        personModel.setName(personDTO.getName());
+        personModel.setEmail(personDTO.getEmail());
+        personModel.setImgUrl(personDTO.getImgUrl());
+        personModel.setAge(personDTO.getAge());
+        personModel.setSurname(personDTO.getSurname());
+        personModel.setTasks(personDTO.getTasks());
+        return personModel;
+    }
+
+    public PersonDTO map(PersonModel personModel) {
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setId(personModel.getId());
+        personDTO.setName(personModel.getName());
+        personDTO.setEmail(personModel.getEmail());
+        personDTO.setImgUrl(personModel.getImgUrl());
+        personDTO.setAge(personModel.getAge());
+        personDTO.setSurname(personModel.getSurname());
+        personDTO.setTasks(personModel.getTasks());
+        return personDTO;
+    }
 }
